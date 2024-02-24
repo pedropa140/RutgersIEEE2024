6e982f43fb8c8ed488f2bc5313cef5cf1d608109
Pedro A. Pajarillo Jr <pedropa140@gmail.com>
2024-02-24 12:57:40 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,1 +1,45 @@
-import flask
+# Standard Library Imports
+import os
+import sqlite3
+from datetime import datetime, timezone
+from os import urandom
+from dotenv import load_dotenv
+import datetime as dt
+import os.path
+import time
+import calendarprogram
+# Third-Party Imports
+from flask import Flask, jsonify, render_template, redirect, request, session, url_for, g
+from datetime import datetime
+
+# External Library Imports
+import google.generativeai as genai
+from google.auth import load_credentials_from_file
+from google.oauth2 import credentials
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google.generativeai import generative_models
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+app = Flask(__name__)
+app.secret_key = urandom(24)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+load_dotenv()
+DATABASE = 'task.db'
+app.config['DATABASE'] = DATABASE
+
+genai_client = None
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+try:
+    api_key = os.getenv("GENAI_API_KEY")
+    if api_key:
+        genai_client = generative_models.GenerativeModelsServiceClient(api_key=api_key)
+    else:
+        print("GENAI_API_KEY environment variable is not set.")
+except Exception as e:
+    print("Error initializing GenAI client:", e)
