64f0a62739e7c07f7cf8e6f8618800b125f6e847
ShatakshiRanjan <shatakshiranjan02@gmail.com>
2024-02-24 18:35:27 -0500

    fixing conections

--- flask/app.py
+++ flask/app.py
@@ -21,6 +21,7 @@ from google.oauth2.credentials import Credentials
 from google.generativeai import generative_models
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+
 from googleapiclient.errors import HttpError
 
 
